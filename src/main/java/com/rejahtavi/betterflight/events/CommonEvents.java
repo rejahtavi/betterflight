5a251d1be8766941f68464322255fb6a7881dfa5
ArkVeil <40077783+ArkVeil@users.noreply.github.com>
2023-12-05 19:38:22 -0500

    Refactored Network Messages
    Reorganized methods and naming to be overall more cohesive.

--- src/main/java/com/rejahtavi/betterflight/events/CommonEvents.java
+++ src/main/java/com/rejahtavi/betterflight/events/CommonEvents.java
@@ -2,7 +2,7 @@ package com.rejahtavi.betterflight.events;
 
 import com.rejahtavi.betterflight.BetterFlight;
 import com.rejahtavi.betterflight.common.BetterFlightCommonConfig;
-import com.rejahtavi.betterflight.network.SElytraChargePacket;
+import com.rejahtavi.betterflight.network.STCElytraChargePacket;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -15,17 +15,17 @@ public class CommonEvents {
 
     @SubscribeEvent
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        SElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
+        STCElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
     }
 
     @SubscribeEvent
     public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        SElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
+        STCElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
     }
     
     @SubscribeEvent
     public static void onPlayerChangeGameMode(PlayerEvent.PlayerChangeGameModeEvent event) {
-        SElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
+        STCElytraChargePacket.send(event.getEntity(), BetterFlightCommonConfig.maxCharge);
     }
 
     // TODO: Possibly a 'get altitude' function, and an option to restrict
