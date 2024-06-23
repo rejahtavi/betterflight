abee419ed76ed73e6cb2a0e8f9e83f78551fe0c1
ArkVeil <40077783+ArkVeil@users.noreply.github.com>
2024-06-23 14:37:27 -0400

    refactor: optimize imports

--- src/main/java/com/rejahtavi/betterflight/compat/CuriosCompat.java
+++ src/main/java/com/rejahtavi/betterflight/compat/CuriosCompat.java
@@ -1,15 +1,9 @@
 package com.rejahtavi.betterflight.compat;
 
 import com.illusivesoulworks.elytraslot.platform.ForgeElytraPlatform;
-import com.illusivesoulworks.elytraslot.platform.services.IElytraPlatform;
-import com.rejahtavi.betterflight.common.BetterFlightCommonConfig;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
-import top.theillusivec4.curios.api.CuriosApi;
-
-import java.util.NoSuchElementException;
 
 public class CuriosCompat
 {
